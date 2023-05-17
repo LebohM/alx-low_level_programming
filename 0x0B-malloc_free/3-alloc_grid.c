a9eaef173a00ea211b61bf5c82345e1003ef034c
LebohM <lebohmofo0812@gmail.com>
2023-05-17 06:15:05 -0700

    function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,9 +6,46 @@
  *alloc_grid -  returns a pointer to a 2 dimensional array of integers.
  *@width: input
  *@height: input
- *return : NULL if failed 
+ *return : NULL if failed
  */
 
 int **alloc_grid(int width, int height)
 {
+	int i, b, c, d;
+	int **a;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+
+		if (a[i] == NULL)
+		{
+			for (b = i; b >= 0; b--)
+			{
+				free(a[b]);
+			}
+
+			free(a);
+			return (NULL);
+		}
+
+	}
+	for (c = 0; c <= height; c++)
+	{
+		for (d = 0; d < width; d++)
+		{
+			a[c][d] = 0;
+		}
+	}
+	return (a);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 
 /**
- *free_grid - 
- *@grid
- *@height
+ *free_grid - frees a 2 dimensional grid
+ *@grid:...
+ *@height: ...
  */
 void free_grid(int **grid, int height)
 {
+	int i;
 
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
